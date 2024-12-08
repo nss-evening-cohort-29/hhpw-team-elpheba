01239238f8d8b5982ccea6a269e17f3396c6f7ba
Ozzy Akben <akbenof@gmail.com>
2024-12-07 20:51:37 -0500

    Update the file structure

--- clearDom.js
+++
@@ -1,7 +0,0 @@
-const clearDom = () => {
-  document.querySelector('#navigation').innerHTML = '';
-  document.querySelector('#login-form-container').innerHTML = '';
-  document.querySelector('#app').innerHTML = '';
-};
-
-export default clearDom;
--- utils/clearDom.js
+++ utils/clearDom.js
@@ -1,5 +1,8 @@
 const clearDOM = () => {
   document.querySelector('#main-container').innerHTML = '';
+  document.querySelector('#navigation').innerHTML = '';
+  document.querySelector('#login-form-container').innerHTML = '';
+  document.querySelector('#app').innerHTML = '';
 };
 
 export default clearDOM;
