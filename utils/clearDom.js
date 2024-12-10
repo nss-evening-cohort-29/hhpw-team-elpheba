544e30d31966b23595568016e0d07d02db562f18
Mike McGee <mmcgee738@gmail.com>
2024-12-09 20:07:00 -0600

    rendering user specific order cards to dom upon clicking view orders in navbar

--- utils/clearDom.js
+++ utils/clearDom.js
@@ -1,8 +1,6 @@
 const clearDOM = () => {
-  document.querySelector('#main-container').innerHTML = '';
-  document.querySelector('#navigation').innerHTML = '';
+  document.querySelector('#orders-container').innerHTML = '';
   document.querySelector('#login-form-container').innerHTML = '';
-  document.querySelector('#app').innerHTML = '';
   document.querySelector('#view').innerHTML = '';
   document.querySelector('#add-button').innerHTML = '';
 };
--- utils/startApp.js
+++ utils/startApp.js
@@ -1,10 +1,14 @@
 import logoutButton from '../components/buttons/logoutButton';
 import navBar from '../components/navbar';
+import navigationEvents from '../events/navigationEvents';
 import homePage from '../pages/homePage';
+import domBuilder from '../shared/domBuilder';
 
 const startApp = (user) => {
   document.querySelector('#login-form-container').innerHTML = ''; // Clear login button
+  domBuilder();
   navBar(user); // Pass user data to navbar
+  navigationEvents(user);
   logoutButton(); // Add logout button to navbar
   homePage(user); // Show homepage with user-specific content
 };
