42142653c3f48fa8e7fd89e2b9cb63183bea1b69
Alyssa Cleland <alyssamariecleland@gmail.com>
2024-12-10 22:24:23 -0600

    Create Order Items Functionality

--- events/domEvents.js
+++ events/domEvents.js
@@ -2,23 +2,28 @@ import { getOrderItems } from '../api/orderItems';
 // eslint-disable-next-line import/extensions
 import { emptyOrderItems, showOrderItems } from '../pages/orderItemsPage';
 import clearDOM from '../utils/clearDom';
+import addOrderItemForm from '../pages/createOrderItemPage';
 
 const domEvents = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
     // CLICK EVENT ON DETAILS BUTTON OF AN ORDER
     if (e.target.id.includes('order-card-details')) {
-      console.warn('clicked details button');
-      console.warn(e.target); // Check the element being clicked
       clearDOM();
       const [, firebaseKey] = e.target.id.split('--');
-      getOrderItems(firebaseKey).then((orders) => {
-        if (orders.length === 0) {
-          emptyOrderItems();
+      getOrderItems(firebaseKey).then((items) => {
+        if (items.length === 0) {
+          emptyOrderItems(firebaseKey);
         } else {
-          showOrderItems(orders);
+          showOrderItems(items, firebaseKey);
         }
       });
     }
+    // CLICK EVENT ON "ADD ITEM" BUTTON ON AN ORDER'S ORDER ITEMS PAGE
+    if (e.target.id.includes('add-item-btn')) {
+      const [, firebaseKey] = e.target.id.split('--'); // destructuring to assign firebasekey of the order, which was passed into showOrderItems to store in the add item button
+      addOrderItemForm(firebaseKey);
+    }
+
     // Below closes out the higher-order addEventListener and domEvents
   });
 };
--- events/formEvents.js
+++ events/formEvents.js
@@ -1,5 +1,7 @@
 import { getOrders, updateOrder, createOrder } from '../api/orders';
 import { showOrders } from '../pages/ordersPage';
+import { createOrderItem, getOrderItems } from '../api/orderItems';
+import { showOrderItems } from '../pages/orderItemsPage';
 
 const formEvents = (user) => {
   document.querySelector('#main-container').addEventListener('submit', (e) => {
@@ -45,6 +47,25 @@ const formEvents = (user) => {
         getOrders(user.uid).then(showOrders);
       });
     }
+    // Click event for submit an order item
+    if (e.target.id.includes('submit-the-orderItem')) {
+      console.warn('clicked add/edit item');
+      const [, orderFirebaseKey] = e.target.id.split('--'); // this will be the firebaseKey of order via the latter condition of the ternary for the id in the form to create an order
+      // we need this to add the order firebaseKey to order_id in payload below so that the order item is associated with its' order
+      console.warn(orderFirebaseKey);
+      const payload = {
+        created_at: Date.now(),
+        item_name: document.querySelector('#item_name').value,
+        item_price: document.querySelector('#item_price').value,
+        menu_item_id: 'placeholder',
+        order_id: orderFirebaseKey,
+        quantity: 1
+      };
+
+      createOrderItem(payload);
+      getOrderItems(orderFirebaseKey).then((items) => showOrderItems(items, orderFirebaseKey));
+    }
+    // below closes the higher order functions
   });
 };
 
