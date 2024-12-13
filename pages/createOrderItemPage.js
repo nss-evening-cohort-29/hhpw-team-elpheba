0835a1247b7f0dae10867903156c38165bc2ae40
Alyssa Cleland <alyssamariecleland@gmail.com>
2024-12-12 21:30:55 -0600

    price input for create order item form now only accepts numerical input and up to 2 decimal places

--- pages/createOrderItemPage.js
+++ pages/createOrderItemPage.js
@@ -6,6 +6,7 @@ const addOrderItemForm = (firebaseKey, obj = {}) => {
   clearDOM();
   // id of the form ternary: The first condition, if the order item already has a firebase key (if it already exists), is for updating. The second condition, if not obj does not yet have a firebase key, is for creating, and it inserts the associated order's firebasekey in the id for use in addOrderItemForm
   // "submit-the-orderItem" may seem like a weird name. but having it as "submit-orderItem" conflicted with other functions in formEvents that targeted id's also containing "submit-order," so please leave as is!
+  // type="number" step="0.01" of the price input: type restricts the input to numerical values, and step allows decimal values with up to two decimal places
   const domString = `
     <form id="${obj.firebaseKey ? `update-orderItem--${obj.firebaseKey}` : `submit-the-orderItem--${firebaseKey}`}" class="mb-4">
       <div class="form-group">
@@ -14,7 +15,7 @@ const addOrderItemForm = (firebaseKey, obj = {}) => {
       </div>
       <div class="form-group">
         <label for="image">Item Price</label>
-        <input type="text" class="form-control" id="item_price" placeholder="Item Price" value="${obj.item_price || ''}" required>
+        <input type="number" step="0.01" class="form-control" id="item_price" placeholder="Item Price" value="${obj.item_price || ''}" required>
       </div>
       <button type="submit" class="btn btn-primary mt-3" id="submit-item-form-btn">Add/Edit Item</button>
     </form>`;
